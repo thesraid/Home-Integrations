0e749f7e7fbd99ea60d3c33ec7784458306b3a96
thesraid <pi@mrRobot.home.lan>
2018-12-23 22:04:20 +0000

    Gave up and will try to get Phlex working Plex ives m3u8 streaming URLs which don't work with chromecast

--- plexVoice/plexVoice.py
+++ plexVoice/plexVoice.py
@@ -1,19 +1,73 @@
 #!/usr/bin/env python
 
 from plexapi.server import PlexServer
-baseurl = 'http://192.168.0.14:32400'
-token = 'DXzLXpZV1uNRrcCPkAzZ'
-plex = PlexServer(baseurl, token)
+import argparse
+import time
+import pychromecast
 
+###########################################################################################
+"""
+Get command line args from the user.
+"""
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='Video type, name name and Chromecast')
 
-'''# Example 1: List all unwatched movies.
-movies = plex.library.section('Movies')
-for video in movies.search(unwatched=True):
-        print(video.title)
+      
+    parser.add_argument('-t', '--type',
+        required=True,
+        action='store',
+        help='TV or Movie')
+    
+    parser.add_argument('-v', '--video',
+        required=True,
+        action='store',
+        help='Video')
+
+    parser.add_argument('-c', '--chromecast',
+        required=False,
+        default="Projector",
+        action='store',
+        help='Chromecast')
+
+    args = parser.parse_args()
+
+    return args
+
+###########################################################################################
+def playOnChromecast( URL, ccName ):
+    print URL
+    chromecasts = pychromecast.get_chromecasts()
+    cast = next(cc for cc in chromecasts if cc.device.friendly_name == ccName)
+    cast.wait()
+    #print(cast.device)
+    mc = cast.media_controller
+    # Plex gives us a m2u8 URL which chromecast can't play natively. GAME OVER MAN, GAME OVER
+    mc.play_media(URL, 'video/mp4')
+    mc.block_until_active()
+    print(mc.status)
+###########################################################################################
+
+'''
+Main Module
 '''
 
-video = plex.library.section('Movies').get('Unbreakable')
-print('Run running the following command to play in Chromecast:')
-print('catt -d Projector cast "%s"' % video.getStreamURL())
+def main():
 
-# Better to use pyChromecast maybe? 
+    args = get_args()
+    mediaType  = args.type
+    videoName = args.video
+    ccName = args.chromecast
+
+    baseurl = 'http://192.168.0.14:32400'
+    token = 'hdHPsyJ8ingKxpzmLcPn'
+    plex = PlexServer(baseurl, token)
+
+    video = plex.library.section(mediaType).get(videoName)
+    playOnChromecast(video.getStreamURL(), ccName)
+
+###########################################################################################
+
+""" Start program """
+if __name__ == "__main__":
+    main()
